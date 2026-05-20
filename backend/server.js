151691a170cada921f02426e5bda1adca283bfb1
banothuvinodkumar <vinodkumarbanothu855@gmail.com>
2026-05-20 18:50:15 +0530

    cmt2

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173"
+      "http://localhost:5173",
+      process.env.FRONTEND_URL // Will be set in Render dashboard later
     ],
     credentials: true
   })
