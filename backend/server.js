6c7db70944742208b9a17c5a107033aa292fc275
banothuvinodkumar <vinodkumarbanothu855@gmail.com>
2026-05-20 19:21:38 +0530

    cmt4

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      process.env.FRONTEND_URL // Will be set in Render dashboard later
+      process.env.FRONTEND_URL,
     ],
     credentials: true
   })
